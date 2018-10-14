4456375c38e0a8bc2cf240afac6bcc69f913f30c
chiaya710623 <chiaya710623@gmail.com>
2018-10-14 23:16:09 +0800

    設定content.html的變數

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-content',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  amount = 0;
-  product_id = null;
-  constructor() {}
-  ngOnInit() {}
+  products = this.dataService.products;
+  id = 1;
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) {}
+  ngOnInit() {
+    this.route.params.subscribe(data => {
+      console.log(data);
+      // 想辦法把data弄出去
+    });
+  }
   add_to_cart() {}
+
 }
