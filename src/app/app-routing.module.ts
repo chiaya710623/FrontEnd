1443771d9e964be5b3c5eedd9ee33710714ea328
chiaya710623 <chiaya710623@gmail.com>
2018-10-14 14:53:15 +0800

    將master的結構弄好

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MasterComponent } from './master/master.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MasterComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
