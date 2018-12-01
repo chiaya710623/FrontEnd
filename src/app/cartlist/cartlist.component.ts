68bbbffe945565d2d20af593be1f588e0d97a99c
chiaya710623 <chiaya710623@gmail.com>
2018-12-01 14:28:11 +0800

    刪掉一些沒有必要的code

--- src/app/cartlist/cartlist.component.ts
+++ src/app/cartlist/cartlist.component.ts
@@ -14,22 +14,22 @@ export class CartlistComponent implements OnInit {
     private productsService: ProductsService
   ) {}
   ngOnInit() {}
-  get showcart() {
-    const cart = [];
-    for (let i = 0; i < this.cartService.cart.length; i++) {
-      for (let j = 0; j < this.productsService.originalProducts.length; j++) {
-        if (this.cartService.cart[i].id === this.productsService.originalProducts[j].id) {
-          cart[i] = this.productsService.originalProducts[j];
-          cart[i].amount = this.cartService.cart[i].item_amount;
-        }
-      }
-    }
-    this.total = 0;
-    for (let i = 0; i < cart.length; i++) {
-      this.total += cart[i].price * cart[i].amount;
-    }
-    return cart;
-  }
+  // get showcart() {
+  //   const cart = [];
+  //   for (let i = 0; i < this.cartService.cart.length; i++) {
+  //     for (let j = 0; j < this.productsService.originalProducts.length; j++) {
+  //       if (this.cartService.cart[i].id === this.productsService.originalProducts[j].id) {
+  //         cart[i] = this.productsService.originalProducts[j];
+  //         cart[i].amount = this.cartService.cart[i].item_amount;
+  //       }
+  //     }
+  //   }
+  //   this.total = 0;
+  //   for (let i = 0; i < cart.length; i++) {
+  //     this.total += cart[i].price * cart[i].amount;
+  //   }
+  //   return cart;
+  // }
   plus(index) {
     if (this.cartService.cart[index].item_amount < 10) {
       this.cartService.cart[index].item_amount++;
