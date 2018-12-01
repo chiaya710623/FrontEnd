52364bd7b09603e826f1efae1dca6837d9e6aee1
chiaya710623 <chiaya710623@gmail.com>
2018-12-01 16:26:39 +0800

    做了註冊

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +7,14 @@ import { Injectable } from '@angular/core';
 export class CartService {
   list_amount = 0;
   cart = [];
-  constructor() {}
-  add_to_cart(id, item_amount) {
+  constructor(private httpClient: HttpClient) {}
+
+  add_to_cart(id, item_amount, stock) {
     if (this.cart !== []) {
       for (let i = 0; i < this.cart.length; i++) {
         if (this.cart[i].id === id) {
-          if (this.cart[i].item_amount + item_amount > 10) {
-            alert('超過訂購數量上限，單項商品最多訂購10件。');
+          if (this.cart[i].item_amount + item_amount > stock) {
+            alert('訂購數量超過庫存量。');
           } else {
             this.cart[i].item_amount += item_amount;
             alert('已增加' + item_amount + '件此商品至購物車中。');
@@ -24,5 +26,8 @@ export class CartService {
     this.cart[this.list_amount] = { id: id, item_amount: item_amount };
     this.list_amount++;
     alert('已加入' + item_amount + '件此商品至購物車。');
+  }
+  getCartlists(member_id) {
+    return this.httpClient.get(`http://host.limaois.me:1723/api/orders?member_id=${member_id}&state=${0}`);
   }
 }
